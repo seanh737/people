67dd91a4d5244eac123e1878b367bcdf36d88475
Sean Hsieh <seanh737@gmail.com>
2019-07-15 11:35:31 +0800

    Show、Index

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PersonResource;
+use App\Http\Resources\PersonResourceCollection;
 use App\Person;
 use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    public function show(Person $person)
+    /**
+     * @param Person $person
+     * @return PersonResource
+     */
+    public function show(Person $person): PersonResource
     {
-        return $person;
+        return new PersonResource($person);
+    }
+
+    /**
+     * @return PersonResourceCollection
+     */
+    public function index():PersonResourceCollection
+    {
+        return new PersonResourceCollection(Person::paginate());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 use App\Person;
-Route::get('/person/{person}','PersonController@show');
+//Route::get('/person/{person}','PersonController@show');
+Route::apiResource('/person','PersonController');
